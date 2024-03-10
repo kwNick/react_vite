08d78b0bd07c355a350a74058cb53bf430d055da
Nickolas Piraino <kwnp17c@gmail.com>
2024-03-10 19:15:19 -0400

    styling of links in nav

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -6,10 +6,10 @@ const Head = () => {
   return (
     <div className='flex flex-col items-center gap-y-4'>
       <h1 className='text-xl text-red-500'>Simple Application!</h1>
-      <div className="px-2 flex gap-x-5 border-b hover:shadow-sm hover:transition-shadow hover:shadow-blue-500">
-        <Link to={"/about"}>About Me</Link>
-        <Link to={"/contact"}>Contact</Link>
-        <Link to={"/projects"}>Projects</Link>
+      <div className="flex gap-x-5">
+        <Link to={"/about"} className='p-2 border-b hover:rounded-xl hover:shadow hover:transition-shadow hover:shadow-slate-500 hover:text-xl duration-300'>About Me</Link>
+        <Link to={"/contact"} className='p-2 border-b hover:rounded-xl hover:shadow hover:transition-shadow hover:shadow-slate-500 hover:text-xl duration-300'>Contact</Link>
+        <Link to={"/projects"} className='p-2 border-b hover:rounded-xl hover:shadow hover:transition-shadow hover:shadow-slate-500 hover:text-xl duration-300'>Projects</Link>
       </div>
     </div>
   )
